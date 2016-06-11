f54f1cc9dfd4f938956d861b3f21a70c5a3d24ea
momdo <xmomdo@gmail.com>
2016-06-12 01:33:58 +0900

    [css-2015] Update source to 2016-06-10 Consider #56, #57 and #59.

--- css-2015/trans-refs.js
+++ css-2015/trans-refs.js
@@ -19,40 +19,44 @@ addEventListener("DOMContentLoaded", function(){
 	// ID : {URL | [URL, ...]}, ...
 	var refList = {
 //css2: 'http://www.y-adagio.com/public/standards/tr_css2/toc.html',
-css21: 'http://momdo.github.io/css2/cover.html',
+css2: 'https://momdo.github.io/css2/Overview.html',
 select: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-selectors/',
 
-"compositing": "http://www.hcn.zaq.ne.jp/___/WEB/compositing-ja.html",
-"css-cascade-3": "http://www.hcn.zaq.ne.jp/___/WEB/css3-cascade-ja.html",
+"compositing": "https://triple-underscore.github.io/compositing-ja.html",
+"css-cascade-3": "https://triple-underscore.github.io/css3-cascade-ja.html",
 //"css-color-4"
-"css-conditional-3": "http://www.hcn.zaq.ne.jp/___/WEB/css3-conditional-ja.html",
-//"css-counter-styles-3"
-"css-fonts-3": "http://www.hcn.zaq.ne.jp/___/WEB/css-fonts-ja.html",
-//"css-style-attr"
-"css-syntax-3": "http://www.hcn.zaq.ne.jp/___/WEB/css-syntax-ja.html",
-"css-ui-3": [ "http://momdo.github.io/css-ui/",
-"http://www.hcn.zaq.ne.jp/___/WEB/css-ui-ja.html",
-],
-"css-values": [ 'http://momdo.github.io/css3-values/',
-	'http://www.hcn.zaq.ne.jp/___/WEB/css-values-ja.html',
-],
-"css3-background": "http://www.hcn.zaq.ne.jp/___/WEB/css-backgrounds-ja.html",
-"css3-color":"http://standards.mitsue.co.jp/resources/w3c/TR/css3-color/",
-"css3-images": 'http://www.hcn.zaq.ne.jp/___/WEB/css-images-ja.html',
-"css3-mediaqueries": "http://standards.mitsue.co.jp/resources/w3c/TR/css3-mediaqueries/",
-"css3-multicol": "http://www.hcn.zaq.ne.jp/___/WEB/css-multicol-ja.html",
-"css3-namespace": [ "http://momdo.s35.xrea.com/web-html-test/spec/REC-css-namespaces-3-20140320.html",
-"http://www.hcn.zaq.ne.jp/___/WEB/css-namespaces-ja.html",
-],
-"css-flexbox-1": "http://www.hcn.zaq.ne.jp/___/WEB/css-flexbox-ja.html",
-//"css-masking-1"
-//"css-shapes-1"
-"css-text-decor-3": "http://momdo.s35.xrea.com/web-html-test/spec/CR-css-text-decor-3-20130801.html",
-"css3-animations": "http://www.hcn.zaq.ne.jp/___/WEB/css-animations-ja.html",
-"css3-speech": "http://www.hcn.zaq.ne.jp/___/WEB/css-speech-ja.html",
-"css3-transforms": "http://www.hcn.zaq.ne.jp/___/WEB/css-transforms-ja.html",
-"css3-transitions": "http://www.hcn.zaq.ne.jp/___/WEB/css-transitions-ja.html",
 
+"css-fonts-3": "https://triple-underscore.github.io/css-fonts-ja.html",
+"css-grid-1": "https://triple-underscore.github.io/css-grid-ja.html",
+
+"css-style-attr": "https://triple-underscore.github.io/css-style-attr-ja.html",
+"css-syntax-3": "https://triple-underscore.github.io/css-syntax-ja.html",
+"css-ui-3": [ "https://momdo.github.io/css-ui/",
+"https://triple-underscore.github.io/css-ui-ja.html",
+],
+"css-values-3": [ 'https://momdo.github.io/css3-values/',
+	'https://triple-underscore.github.io/css-values-ja.html',
+],
+"css3-background": "https://triple-underscore.github.io/css-backgrounds-ja.html",
+"css3-color":"http://standards.mitsue.co.jp/resources/w3c/TR/css3-color/",
+"css3-conditional": "https://triple-underscore.github.io/css-conditional-ja.html",
+"css3-images": 'https://triple-underscore.github.io/css-images-ja.html',
+"css3-mediaqueries": "http://standards.mitsue.co.jp/resources/w3c/TR/css3-mediaqueries/",
+"css3-multicol": "https://triple-underscore.github.io/css-multicol-ja.html",
+"css3-namespace": [ "http://momdo.s35.xrea.com/web-html-test/spec/REC-css-namespaces-3-20140320.html",
+"https://triple-underscore.github.io/css-namespaces-ja.html",
+],
+"css-flexbox-1": "https://triple-underscore.github.io/css-flexbox-ja.html",
+//"css-masking-1"
+"css-shapes-1": "https://triple-underscore.github.io/css-shapes-ja.html",
+"css-text-decor-3": ["https://momdo.github.io/css-text-decor-3/",
+"https://triple-underscore.github.io/css-text-ja.html"],
+"css3-animations": "https://triple-underscore.github.io/css-animations-ja.html",
+"css3-speech": "https://triple-underscore.github.io/css-speech-ja.html",
+"css3-transforms": "https://triple-underscore.github.io/css-transforms-ja.html",
+"css3-transitions": "https://triple-underscore.github.io/css-transitions-ja.html",
+"selectors4": "https://triple-underscore.github.io/selectors4-ja.html",
+"css-will-change-1": "https://triple-underscore.github.io/css-will-change-ja.html",
 	};
 
 	(function(){
