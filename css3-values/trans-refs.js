4f90aa06a71180959719da4369d0456b4efa391a
momdo <xmomdo@gmail.com>
2016-06-19 09:41:54 +0900

    [css3-values] Update source to 2016-06-17 ED

--- css3-values/trans-refs.js
+++ css3-values/trans-refs.js
@@ -18,40 +18,46 @@ addEventListener("DOMContentLoaded", function(){
 	// URL リスト
 	// ID : {URL | [URL, ...]}, ...
 	var refList = {
+"compositing-1": 'https://triple-underscore.github.io/compositing-ja.html',
+"css-backgrounds-3": 'https://triple-underscore.github.io/css-backgrounds-ja.html',
+"css-cascade-4": 'https://triple-underscore.github.io/css-cascade-ja.html',
+"css-color-3": 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-color/',
+"css-color-4": 'https://triple-underscore.github.io/css-color-ja.html',
+"css-counter-styles-3": 'https://triple-underscore.github.io/css-counter-styles-ja.html',
+"css-display-3": 'https://triple-underscore.github.io/css-display-ja.html',
+"css-images-3": 'https://triple-underscore.github.io/css-images-ja.html',
+//overflow-4
+"css-writing-modes-3": "https://triple-underscore.github.io/css-writing-modes-ja.html",
 css21: 'https://momdo.github.io/css2/cover.html',
-css3bg: 'http://www.hcn.zaq.ne.jp/___/WEB/css-backgrounds-ja.html',
-css3cascade: 'http://www.hcn.zaq.ne.jp/___/WEB/css-cascade-ja.html',
-css3color: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-color/',
+"css3-fonts": 'https://triple-underscore.github.io/css-fonts-ja.html',
+//CSS3-IMAGES
+//CSS3COLOR
 css3namespace: 'http://momdo.s35.xrea.com/web-html-test/spec/REC-css-namespaces-3-20140320.html',
-css3page: 'http://www.hcn.zaq.ne.jp/___/WEB/css-page-ja.html',
-css3syn: 'http://www.hcn.zaq.ne.jp/___/WEB/css-syntax-ja.html',
-"compositing-1": 'http://www.hcn.zaq.ne.jp/___/WEB/compositing-ja.html',
-"css-animations-1": 'http://www.hcn.zaq.ne.jp/___/WEB/css-animations-ja.html',
-"css-break-3": 'http://www.hcn.zaq.ne.jp/___/WEB/css-break-ja.html',
-"css-cascade-4": 'http://www.hcn.zaq.ne.jp/___/WEB/css-cascade-ja.html',
-"css-fonts-3": 'http://www.hcn.zaq.ne.jp/___/WEB/css-fonts-ja.html',
-"css-images-3": 'http://www.hcn.zaq.ne.jp/___/WEB/css-images-ja.html',
-"css-text-3": 'http://www.hcn.zaq.ne.jp/___/WEB/css-text-ja.html',
-"css-text-decor-3": 'https://momdo.github.io/css-text-decor-3/',
-"css-transitions-1": 'http://www.hcn.zaq.ne.jp/___/WEB/css-transitions-ja.html',
-"css-ui-3": ['https://momdo.github.io/css-ui/',
-    'http://www.hcn.zaq.ne.jp/___/WEB/css-ui-ja.html',
-],
-"whatwg-dom": 'http://www.hcn.zaq.ne.jp/___/WEB/DOM4-ja.html',
-"whatwg-url": 'http://www.hcn.zaq.ne.jp/___/WEB/URL-ja.html',
-"mediaqueries-4": 'http://www.hcn.zaq.ne.jp/___/WEB/mediaqueries4-ja.html',
-
-"mediaq": 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-mediaqueries/',
-"css-writing-modes-3": "http://www.hcn.zaq.ne.jp/___/WEB/css-writing-modes-ja.html",
-
-"cssom-1": 'http://www.hcn.zaq.ne.jp/___/WEB/cssom-ja.html',
+css3page: 'https://triple-underscore.github.io/css-page-ja.html',
+css3syn: 'https://triple-underscore.github.io/css-syntax-ja.html',
+"cssom": 'https://triple-underscore.github.io/cssom-ja.html',
 rfc2119: [ 'http://www.ipa.go.jp/security/rfc/RFC2119JA.html',
           'http://www.studyinghttp.net/rfc_ja/rfc2119',
           'http://www.cam.hi-ho.ne.jp/mendoxi/rfc/rfc2119j.html',
           'http://www.asahi-net.or.jp/~sd5a-ucd/rfc-j/rfc-2119j.html',
           'http://www.t-net.ne.jp/~cyfis/rfc/format/rfc2119_ja.html'
 ],
-"selectors-4": 'http://www.hcn.zaq.ne.jp/___/WEB/selectors4-ja.html',
+"selectors-4": 'https://triple-underscore.github.io/selectors4-ja.html',
+"url": 'https://triple-underscore.github.io/URL-ja.html',
+"whatwg-dom": 'https://triple-underscore.github.io/DOM4-ja.html',
+
+"css-animations-1": 'https://triple-underscore.github.io/css-animations-ja.html',
+"css-break-3": 'https://triple-underscore.github.io/css-break-ja.html',
+"css-text-3": 'https://triple-underscore.github.io/css-text-ja.html',
+"css-text-decor-3": ['https://momdo.github.io/css-text-decor-3/', 'https://triple-underscore.github.io/css-text-ja.html'],
+"css-transitions-1": 'https://triple-underscore.github.io/css-transitions-ja.html',
+"css-ui-3": ['https://momdo.github.io/css-ui/',
+    'https://triple-underscore.github.io/css-ui-ja.html',
+],
+"html": 'https://momdo.github.io/html/',
+"mediaq": 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-mediaqueries/',
+
+//"mediaqueries-4": 'https://triple-underscore.github.io/mediaqueries4-ja.html',
 
 	};
 
