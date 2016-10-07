ade22819ed7e67a09ff691f5cc223a0725abb787
momdo <xmomdo@gmail.com>
2016-10-07 20:54:41 +0900

    [css2] Update js

--- css2/trans-refs.js
+++ css2/trans-refs.js
@@ -1,12 +1,14 @@
 /*
   Japanese translation Reference
   2013-12-11 ver.
-  (2015-06-27 customized. by https://github.com/momdo/momdo.github.io/pull/4)
-  Copyright (c) 2013
+  (2016-10-07 customized for CSS2.)
+
+  Copyright (c) 2013, 2016
       Makoto Mizukami,
         <http://makotom.org/>
       momdo
         <http://momdo.s35.xrea.com/>.
+
   licence: GNU-GPL-3.0-or-later
   // @source: http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -17,36 +19,31 @@ addEventListener("DOMContentLoaded", function(){
 	// ID : {URL | [URL, ...]}, ...
 	var refList = {
 HTML4: 'http://www.asahi-net.or.jp/~sd5a-ucd/rec-html401j/cover.html',
-PNG: 'http://www.milk-island.net/document/png/',
-RFC3986: 'http://www.eonet.ne.jp/~h-hash/rfc_ja/rfc3986.ja.html',
-RFC2045: ['http://www.darts.jp/reference/rfc/rfc2045-jp.txt',
-'http://www.t-net.ne.jp/~cyfis/rfc/mime/rfc2045_ja-1.html',
-'http://hp.vector.co.jp/authors/VA014833/rfc2045J.html',
+RFC3986: 'https://triple-underscore.github.io/RFC3986-ja.html',
+RFC2045:[ 'http://www.akanko.net/marimo/data/rfc/rfc2045-jp.txt',
+          'http://hp.vector.co.jp/authors/VA014833/rfc2045J.html',
 ],
-RFC2616: ['http://www.spencernetwork.org/reference/rfc2616-ja-HTTP1.1.txt',
-'http://www.eonet.ne.jp/~h-hash/rfc_ja/rfc2616.ja.html',
-'http://hp.vector.co.jp/authors/VA014833/rfc2045J.html',
+RFC2119: [ 'http://www.ipa.go.jp/security/rfc/RFC2119JA.html',
+    'http://www.studyinghttp.net/rfc_ja/rfc2119',
+    'http://www.cam.hi-ho.ne.jp/mendoxi/rfc/rfc2119j.html',
+    'http://www.asahi-net.or.jp/~sd5a-ucd/rfc-j/rfc-2119j.html',
+    'http://www.t-net.ne.jp/~cyfis/rfc/format/rfc2119_ja.html'
 ],
-rfc2119: [ 'http://www.ipa.go.jp/security/rfc/RFC2119JA.html',
-          'http://www.studyinghttp.net/rfc_ja/rfc2119',
-          'http://www.cam.hi-ho.ne.jp/mendoxi/rfc/rfc2119j.html',
-          'http://www.asahi-net.or.jp/~sd5a-ucd/rfc-j/rfc-2119j.html',
-          'http://www.t-net.ne.jp/~cyfis/rfc/format/rfc2119_ja.html'
+2318: [ 'http://www.cam.hi-ho.ne.jp/mendoxi/rfc/rfc2318j.html',
+           'http://suika.fam.cx/~wakaba/wiki/sw/n/RFC%202318',
+           'http://www.7key.jp/rfc/rfc2318.html',
 ],
-RFC2318: ['http://www.cam.hi-ho.ne.jp/mendoxi/rfc/rfc2318j.html',
-'http://www.7key.jp/rfc/rfc2318.html',
+CSS:[ 'https://momdo.github.io/css-2015/',
+      'https://triple-underscore.github.io/css-2015-ja.html',
 ],
-UAAG10: 'http://web.archive.org/web/20141006181205/http://fuji.u-shizuoka-ken.ac.jp/~ishikawa/uaag10-j.htm',
-XML10: 'http://w4ard.eplusx.net/translation/W3C/REC-xml-20081126/',
-CSS1: 'http://www.doraneko.org/webauth/css1/19961217/Overview.html',
-CSS2: 'http://www.y-adagio.com/public/standards/tr_css2/toc.html',
-css3color: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-color/',
+CSS3COLOR: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-color/',
 CSS3SEL: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-selectors/',
-CSS3SPEECH: 'http://www.hcn.zaq.ne.jp/___/WEB/css-speech-ja.html',
-mediaq: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-mediaqueries/',
-SVG11: 'http://www.hcn.zaq.ne.jp/___/SVG11-2nd/',
+CSS3SPEECH: 'https://triple-underscore.github.io/css-speech-ja.html',
+MEDIAQ: 'http://standards.mitsue.co.jp/resources/w3c/TR/css3-mediaqueries/',
+SVG11: 'https://triple-underscore.github.io/SVG11/',
 WCAG20: 'http://waic.jp/docs/WCAG20/Overview.html',
 XHTML: 'http://msugai.fc2web.com/web/W3C/xhtml1SE/Cover.html',
+XMLNAMESPACES: 'http://www.hcn.zaq.ne.jp/___/XML/Namespaces-ja.html',
 	};
 
 	(function(){
@@ -59,20 +56,19 @@ XHTML: 'http://msugai.fc2web.com/web/W3C/xhtml1SE/Cover.html',
 
 			a.setAttribute("href", href);
 			a.appendChild(document.createTextNode(text));
-
+			if(text.match(/訳$/) || text.match(/1$/)){ //最初だけ（強引に）マッチング
+            	span.appendChild(document.createElement("br"));
+			}
 			span.setAttribute("style", "margin-right:5px;");
 			span.appendChild(a);
-
-			return parent.appendChild(span);
+			return parent.parentNode.parentNode.nextElementSibling.appendChild(span); //parent.appendChild(span);
 		};
 
 		for(id in refList){	// リストを巡回
 			if(refList.hasOwnProperty(id)){	// "おまじない"
-				if(dd = document.getElementsByName("ref-" + id.toUpperCase())[0]){	// 当該 ID を持つ要素があることを最初に確認
-					dd = dd.parentNode.parentNode.nextElementSibling;
+				if(dd = document.getElementsByName("ref-" + id)[0]){	// 当該 ID を持つ要素があることを最初に確認
 					urlType = typeof refList[id];
 
-					dd.appendChild(document.createElement("br"));
 					if(urlType === typeof ""){	// URL が1個だけのとき
 						insertA(dd, refList[id], "日本語訳");
 					}else if(urlType === typeof []){	// URL が複数あるとき
